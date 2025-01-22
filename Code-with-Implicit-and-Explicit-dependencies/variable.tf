8b8549e22dfa4eb613510fe68867ccc77ba01b6b
Mahesh-Gontla <115606905+Mahesh-Gontla@users.noreply.github.com>
2025-01-22 13:08:39 +0530

    Create variable.tf

---
+++ Code-with-Implicit-and-Explicit-dependencies/variable.tf
@@ -0,0 +1,25 @@
+variable "aws_region" {}
+
+variable "vpc_cidr_block" {}
+
+variable "vpc_tag" {}
+
+variable "subnet_cidr_block" {}
+
+variable "subent_az" {}
+
+variable "subnet_tag" {}
+
+variable "igw_tag" {}
+
+variable "rt_cidr_block" {}
+
+variable "rt_tag" {}
+
+variable "sg_tag" {}
+
+variable "ec2_az" {}
+
+variable "ec2_instance_type" {}
+
+variable "key_name" {}

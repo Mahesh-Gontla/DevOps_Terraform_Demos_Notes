60d743ce73c68b431b6ebb5d5022222344c52b35
Mahesh-Gontla <115606905+Mahesh-Gontla@users.noreply.github.com>
2025-01-24 12:23:01 +0530

    Update main.tf

--- Code-with-Implicit-and-Explicit-dependencies/main.tf
+++ Code-with-Implicit-and-Explicit-dependencies/main.tf
@@ -71,6 +71,13 @@ resource "aws_security_group" "MySG-Using-TF-Allow-All" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+ ingress {
+    description = "Allow HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     description = "Allow all outbound traffic"
